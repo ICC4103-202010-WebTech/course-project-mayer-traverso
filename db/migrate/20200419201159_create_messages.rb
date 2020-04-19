421cd91ce431ada0ce6945f260a16418f1f8e5e2
GainfrancoTP <gstraverso@miuandes.cl>
2020-04-19 16:52:59 -0400

    minor fix

--- db/migrate/20200419201159_create_messages.rb
+++ db/migrate/20200419201159_create_messages.rb
@@ -3,7 +3,6 @@ class CreateMessages < ActiveRecord::Migration[6.0]
     create_table :messages do |t|
       t.string :text
       t.datetime :date
-      t.integer :user_creator
       t.references :user
 
       t.timestamps
