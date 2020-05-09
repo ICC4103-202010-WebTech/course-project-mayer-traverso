536453712378cbe3d0db236c331d421bf0263d1f
itmayer <itmayer@miuandes.cl>
2020-05-09 17:55:26 -0400

    home

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :set_user
+
+  private
+
+  def set_user
+    @user = User.first
+  end
+  def home
+    @events = Event.all
+  end
 end
