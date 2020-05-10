80a555d5f235b50da75acb6b0728c5c0af6a9022
itmayer <itmayer@miuandes.cl>
2020-05-10 14:37:02 -0400

    profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    @users = User.find(2).profile_page
+    @users = User.all
   end
 
   # GET /users/1
