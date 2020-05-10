127814a3b12b24d68e254213a7d2c47fc419f563
itmayer <itmayer@miuandes.cl>
2020-05-09 20:16:29 -0400

    navbar

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -10,6 +10,7 @@ class OrganizationsController < ApplicationController
   # GET /organizations/1
   # GET /organizations/1.json
   def show
+    @organizations = Organization.all
   end
 
   # GET /organizations/new
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "application#home"
 
-  namespace :api, defaults: { format: :json } do
-    namespace :v1 do
 
-    end
+  resources :organizations, shallow:true, only: [:show, :index]
+  resources :users do
+    resources :events, shallow: true, only: [:show]
   end
 end
