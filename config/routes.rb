64c105d23df2af5318b54d0c9e93814f0bc29aae
GainfrancoTP <gstraverso@miuandes.cl>
2020-05-12 18:37:46 -0400

    message fix

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "application#home"
 
-  resources :events, shallow: true, only: :show
+  resources :events, shallow: true do
+    resources :comments, shallow: true
+  end
   resources :messages, shallow: true, only: :show
   resources :organizations, shallow: true, only: [:show, :index]
   resources :users, shallow: true, only: [:show] do
@@ -14,4 +16,5 @@ Rails.application.routes.draw do
   resources :events, defaults: { format: :html }
   resources :users, defaults: { format: :html }
   resources :organizations, defaults: { format: :html }
+  resources :comments, defaults: { format: :html }
 end
